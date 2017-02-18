1e8a0efea64d8c5906e2068defe41b1bc4d34a5d
Lex Robinson <lex@lex.me.uk>
2017-02-18 22:32:08 +0000

    Visual studio crap

---
+++ Nettest/Nettest.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Nettest.ino
+ Created:	2/18/2017 10:21:09 PM
+ Author:	Lex
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
