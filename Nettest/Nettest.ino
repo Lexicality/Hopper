3317bd376007caccf352c6d128918ca7542b598b
Lex Robinson <lex@lex.me.uk>
2017-02-21 19:50:47 +0000

    NETWORKING

--- Nettest/Nettest.ino
+++ Nettest/Nettest.ino
@@ -4,36 +4,61 @@
  Author:	Lex
 */
 
-int i;
+#include <Ethernet.h>
 
+// Copied from the bottom of the board
+byte mac[] = { 0x00, 0x1A, 0xB6, 0x02, 0xCB, 0x52 };
+
+EthernetClient client;
+
+void setup() {
+	Serial.begin(9600);
+	Serial.println("=============================");
+
+	if (Ethernet.begin(mac) == 0) {
+		Serial.println("Init Ethernet failed");
+		while (true);
+	}
+
+	Serial.print("Ethernet address: ");
+	Serial.println(Ethernet.localIP());
+
+
+	Serial.println("Connecting...");
+	if (client.connect("172.31.24.207", 8081) == 1) {
+		Serial.println("Connected");
+		client.println("Hello World!");
+	} else {
+		Serial.println("Not Connected");
+	}
+
+	Serial.println("We survived the bootup sequence!");
+}
+
+bool oops = false;
+
+void loop() {
+	if (client.available()) {
+		char c = client.read();
+		Serial.print(c);
+	}
+
+	if (!client.connected() && !oops) {
+		oops = true;
+		Serial.println("DED");
+	}
+}
+
+/*
 // the setup function runs once when you press reset or power the board
 void setup() {
-	i = 0;
 	pinMode(D1_LED, OUTPUT);
 	pinMode(D2_LED, OUTPUT);
-	pinMode(D3_LED, OUTPUT);
-	pinMode(D4_LED, OUTPUT);
 }
 
 // the loop function runs over and over again until power down or reset
 void loop() {
-	int led = D1_LED;
-	switch (i) {
-	case 0:
-		led = D1_LED;
-		break;
-	case 1:
-		led = D2_LED;
-		break;
-	case 2:
-		led = D3_LED;
-		break;
-	case 3:
-		led = D4_LED;
-		break;
-	}
-	digitalWrite(led, HIGH);
-	delay(500);
-	digitalWrite(led, LOW);
-	i = (i+1) % 4;
+	client.monitor();
 }
+
+*/
